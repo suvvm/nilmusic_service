6cca57c808886da3c35873c8df45976d8d925a8f
suvvm <suvvm@suvvmdeMacBook-Air.local>
2020-11-18 17:28:20 +0800

    add run server

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+)
 
 func main() {
 	r := gin.Default()
 	register(r)
+	err := r.Run()
+	if err != nil {
+		log.Println("run api service fail")
+	}
 }
